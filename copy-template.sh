ce325ba6dd81b25a343f55d4422caa2aa261f5d9
Brian Mearns <mearns.b@gmail.com>
2020-09-21 20:52:10 -0400

    Add copy-template script

---
+++ copy-template.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+rsync -r \
+    --exclude=.git \
+    --exclude=node_modules/ \
+    --exclude=out/ \
+    --exclude=.nyc_output/ \
+    --exclude=coverage/ \
+    --exclude=copy-template.sh \
+    . "$1"
